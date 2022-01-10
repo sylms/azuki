2c07e305670fdfe322294cac6125e951d009ffec
sobadon <sbdn@chasoba.net>
2022-01-11 03:09:42 +0900

    CourseQuery 構造体にコメントを追加

--- domain/course.go
+++ domain/course.go
@@ -27,22 +27,28 @@ type Course struct {
 }
 
 type CourseQuery struct {
-	CourseNumber             string `json:"course_number"`
+	CourseNumber string `json:"course_number"`
+	// スペース区切り
+	// CourseNameFilterType も指定する
 	CourseName               string `json:"course_name"`
 	InstructionalType        int    `json:"instructional_type"`
 	Credits                  string `json:"credits"`
 	StandardRegistrationYear int    `json:"standard_registration_year"`
 	Term                     string `json:"term"`
-	Period                   string `json:"period"`
-	Classroom                string `json:"classroom"`
-	Instructor               string `json:"instructor"`
+	// csv2sql/kdb の PeriodParser で認識できる形式であれば良い
+	Period     string `json:"period"`
+	Classroom  string `json:"classroom"`
+	Instructor string `json:"instructor"`
+	// スペース区切り
+	// CourseOverviewFilterType も指定する
 	CourseOverview           string `json:"course_overview"`
 	Remarks                  string `json:"remarks"`
 	CourseNameFilterType     string `json:"course_name_filter_type"`
 	CourseOverviewFilterType string `json:"course_overview_filter_type"`
 	FilterType               string `json:"filter_type"`
-	Limit                    int    `json:"limit"`
-	Offset                   int    `json:"offset"`
+	// 必須
+	Limit  int `json:"limit"`
+	Offset int `json:"offset"`
 }
 
 type Facet struct {
